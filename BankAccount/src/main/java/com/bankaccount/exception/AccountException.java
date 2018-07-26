48a19e9926d6807f65005e6d18a92b06f6411b97
emchtiwi <emchtiwi@ITEM-S29175.dhcp.aix.fr.sopra>
2018-07-26 19:29:27 +0200

    Added Account class Exception Account implementation

---
+++ BankAccount/src/main/java/com/bankaccount/exception/AccountException.java
@@ -0,0 +1,14 @@
+package com.bankaccount.exception;
+
+public class AccountException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AccountException(final String message) {
+		super(message);
+	}
+
+}
--- BankAccount/src/main/java/com/bankaccount/metier/Account.java
+++ BankAccount/src/main/java/com/bankaccount/metier/Account.java
@@ -1,23 +1,45 @@
 package com.bankaccount.metier;
 
+import java.time.LocalDateTime;
+
+import com.bankaccount.exception.AccountException;
+import com.bankaccount.service.Iprinter;
+
 public class Account {
 
-	public Account(Amount amount) {
-		throw new UnsupportedOperationException();
+	private Statement statement;
+
+	public Account(Amount a) throws AccountException {
+		if (a == null) {
+			throw new AccountException("The initial balance cannot be null");
+		}
+		statement = new Statement();
 	}
 
-	public void deposit(Amount amount) {
-		throw new UnsupportedOperationException();
+	public void deposit(Amount a) throws AccountException {
+		if (a == null) {
+			throw new AccountException("deposited amount should not be null");
+		}
+
+		statement.addDeposit(a, LocalDateTime.now());
 	}
 
-	public void withdrawal(Amount amount) {
-		throw new UnsupportedOperationException();
-		
+	public void withdrawal(Amount a) throws AccountException {
+		if (a == null) {
+			throw new AccountException("withdrawal amount should not be null");
+		}
+
+		statement.addWithdrawal(a, LocalDateTime.now());
+
 	}
 
-	public void printStatement(Object object) {
-		throw new UnsupportedOperationException();
-		
+	public void print(Iprinter p) {
+		statement.print(p);
+
+	}
+
+	public Amount totalBalance() {
+		return statement.totalBalance();
 	}
 
 }
