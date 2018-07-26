467c3e96d297efc568ee2efbd9702d3ab694598e
emchtiwi <emchtiwi@ITEM-S29175.dhcp.aix.fr.sopra>
2018-07-26 19:07:30 +0200

    Exception Amount implementation

---
+++ BankAccount/src/main/java/com/bankaccount/exception/AmountException.java
@@ -0,0 +1,13 @@
+package com.bankaccount.exception;
+
+public class AmountException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AmountException(final String message) {
+		super(message);
+	}
+}
