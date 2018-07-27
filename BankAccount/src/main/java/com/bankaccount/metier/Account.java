c9821f4de4f7586b4534a55a46b71c1c92e0269e
emchtiwi <emchtiwi@ITEM-S29175.dhcp.aix.fr.sopra>
2018-07-27 11:19:47 +0200

    Bug fix and in Account and Reformatted code

--- BankAccount/src/main/java/com/bankaccount/metier/Account.java
+++ BankAccount/src/main/java/com/bankaccount/metier/Account.java
@@ -3,23 +3,43 @@ package com.bankaccount.metier;
 import java.time.LocalDateTime;
 
 import com.bankaccount.exception.AccountException;
+import com.bankaccount.exception.AmountException;
+import com.bankaccount.exception.TransactionException;
 import com.bankaccount.service.Iprinter;
 
 public class Account {
 
 	private Statement statement;
 
-	public Account(Amount amount) throws AccountException {
+	/**
+	 * Constructor
+	 */
+	public Account(Amount amount) throws AccountException, AmountException, TransactionException {
 		if (amount == null) {
 			throw new AccountException("The initial balance cannot be null");
 		}
 		this.statement = new Statement();
+		this.statement.addDeposit(amount, LocalDateTime.now());
+	}
+
+	/*
+	 * Method to print statement
+	 */
+	public void print(Iprinter print) {
+		this.statement.print(print);
+	}
+
+	/**
+	 * Method to get total balance
+	 */
+	public Amount totalBalance() throws AmountException, TransactionException {
+		return statement.totalBalance();
 	}
 
 	/*
 	 * Method to deposit an amount
 	 */
-	public void deposit(Amount amount) throws AccountException {
+	public void deposit(Amount amount) throws AccountException, AmountException, TransactionException {
 		if (amount == null) {
 			throw new AccountException("deposited amount should not be null");
 		}
@@ -30,7 +50,7 @@ public class Account {
 	/*
 	 * Method to withdrawal an amount
 	 */
-	public void withdrawal(Amount amount) throws AccountException {
+	public void withdrawal(Amount amount) throws AccountException, AmountException, TransactionException {
 		if (amount == null) {
 			throw new AccountException("withdrawal amount should not be null");
 		}
@@ -39,22 +59,7 @@ public class Account {
 
 	}
 
-	/*
-	 * Method to print statement
-	 */
-	public void print(Iprinter print) {
-		this.statement.print(print);
-
-	}
-
-	/**
-	 * Method to get total balance
-	 */
-	public Amount totalBalance() {
-		return statement.totalBalance();
-	}
-
-	Statement statement() {
+	public Statement statement() {
 		return this.statement;
 	}
 
--- BankAccount/src/test/java/com/bankaccount/AccountTest.java
+++ BankAccount/src/test/java/com/bankaccount/AccountTest.java
@@ -1,21 +1,39 @@
 package com.bankaccount;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 
 import com.bankaccount.exception.AccountException;
 import com.bankaccount.exception.AmountException;
+import com.bankaccount.exception.TransactionException;
 import com.bankaccount.metier.Account;
 import com.bankaccount.metier.Amount;
 
 public class AccountTest {
 
 	@Test(expected = AccountException.class)
-	public void balance_should_not_be_null() throws AccountException {
+	public void balance_should_not_be_null() throws AccountException, AmountException, TransactionException {
 		new Account(null);
 	}
 
+	@Test
+	public void statement_should_not_be_null() throws AccountException, AmountException, TransactionException {
+		
+		/**
+		 * Given
+		 */
+		Account account = new Account(new Amount(100f));
+
+		/**
+		 * Then
+		 */
+		assertThat(account.statement()).isNotNull();
+
+	}
+
 	@Test(expected = AccountException.class)
-	public void deposited_amount_should_not_be_null() throws AccountException, AmountException {
+	public void deposited_amount_should_not_be_null() throws AccountException, AmountException, TransactionException {
 
 		/**
 		 * Given
@@ -29,7 +47,7 @@ public class AccountTest {
 	}
 
 	@Test(expected = AccountException.class)
-	public void withdrawal_amount_should_not_be_null() throws AmountException, AccountException {
+	public void withdrawal_amount_should_not_be_null() throws AmountException, AccountException, TransactionException {
 
 		/**
 		 * Given
@@ -41,9 +59,9 @@ public class AccountTest {
 		 */
 		account.withdrawal(null);
 	}
-
-	@Test(expected = UnsupportedOperationException.class)
-	public void deposited_amount_should_be_added_to_the_current_balance() throws AmountException, AccountException {
+	@Test()
+	public void deposited_amount_should_be_added_to_the_current_balance()
+			throws AmountException, AccountException, TransactionException {
 
 		/**
 		 * Given
@@ -56,22 +74,30 @@ public class AccountTest {
 		 */
 		account.deposit(amount);
 
-	}
+		/**
+		 * Then
+		 */
+		assertThat(account.totalBalance()).isEqualTo(new Amount(200f));
 
-	@Test(expected = UnsupportedOperationException.class)
+	}
+	@Test()
 	public void withdrawal_amount_should_be_subtracted_from_the_current_amount()
-			throws AmountException, AccountException {
+			throws AmountException, AccountException, TransactionException {
 
 		/**
 		 * Given
 		 */
 		Account account = new Account(new Amount(300f));
 		Amount amount = new Amount(100f);
-
 		/**
 		 * When
 		 */
 		account.withdrawal(amount);
+
+		/**
+		 * Then
+		 */
+		assertThat(account.totalBalance()).isEqualTo(new Amount(200f));
 
 	}
 
