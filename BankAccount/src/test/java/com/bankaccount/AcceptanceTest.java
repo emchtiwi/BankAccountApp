fe00924ada7c1bec2d2a61db418503f3e840ee83
emchtiwi <emchtiwi@ITEM-S29175.dhcp.aix.fr.sopra>
2018-07-31 22:28:21 +0200

    Updated Acceptance test

--- BankAccount/src/test/java/com/bankaccount/AcceptanceTest.java
+++ BankAccount/src/test/java/com/bankaccount/AcceptanceTest.java
@@ -9,28 +9,29 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.bankaccount.exception.BankAccountException;
-import com.bankaccount.metier.Account;
-import com.bankaccount.metier.Amount;
+import com.bankaccount.pojo.Amount;
+import com.bankaccount.service.metier.AccountMetier;
+import com.bankaccount.service.metier.StatementMetier;
 
 public class AcceptanceTest {
 
 	@Test
-	public void should_generate_a_transaction_history() throws BankAccountException{
+	public void should_generate_a_transaction_history() {
 
 		/*
 		 * Given
 		 */
 		List<String> statements = new ArrayList<>();
 		String toDay = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE);
-		Account account = new Account(new Amount(200.4f));
+		AccountMetier accountMetier = new AccountMetier(new StatementMetier());
 
 		/*
 		 * When
 		 */
-		account.deposit(new Amount(6384.6f));
-		account.withdrawal(new Amount(500f));
-		account.print(statements::add);
+		accountMetier.deposit(new Amount(200.4f));
+		accountMetier.deposit(new Amount(6384.6f));
+		accountMetier.withdrawal(new Amount(500f));
+		accountMetier.print(statements::add);
 
 		/*
 		 * Then
@@ -38,8 +39,8 @@ public class AcceptanceTest {
 		assertThat(statements).isNotEmpty();
 		assertThat(statements).hasSize(4);
 		assertThat(statements.get(0)).isEqualTo(" OPERATION TYPE          AMOUNT         BALANCE  OPERATION DATE");
-		assertThat(statements.get(1)).isEqualTo("        DEPOSIT           200.4             0.0      " + toDay + "");
-		assertThat(statements.get(2)).isEqualTo("        DEPOSIT          6384.6           200.4      " + toDay + "");
-		assertThat(statements.get(3)).isEqualTo("      WITHDRAWL           500.0          6585.0      " + toDay + "");
+		assertThat(statements.get(1)).isEqualTo("        DEPOSIT           200.4           200.4      " + toDay + "");
+		assertThat(statements.get(2)).isEqualTo("        DEPOSIT          6384.6          6585.0      " + toDay + "");
+		assertThat(statements.get(3)).isEqualTo("      WITHDRAWL          -500.0          6085.0      " + toDay + "");
 	}
 }
