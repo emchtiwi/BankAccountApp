134edba926c6b908516b69b0029a0d7ed33fc638
emchtiwi <emchtiwi@ITEM-S29175.dhcp.aix.fr.sopra>
2018-07-31 22:59:26 +0200

    Updated account test

--- BankAccount/src/test/java/com/bankaccount/AccountTest.java
+++ BankAccount/src/test/java/com/bankaccount/AccountTest.java
@@ -1,102 +1,125 @@
 package com.bankaccount;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import com.bankaccount.exception.BankAccountException;
-import com.bankaccount.metier.Account;
-import com.bankaccount.metier.Amount;
+import com.bankaccount.persistance.TransactionPersistance;
+import com.bankaccount.pojo.Account;
+import com.bankaccount.pojo.Amount;
+import com.bankaccount.service.PrinterService;
+import com.bankaccount.service.metier.AccountMetier;
+import com.bankaccount.service.metier.StatementMetier;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AccountTest {
 
-	@Test(expected = BankAccountException.class)
-	public void balance_should_not_be_null() throws BankAccountException {
+	private AccountMetier accountMetier;
+
+	private PrinterService print;
+
+	@Mock
+	private StatementMetier statementMetier;
+
+	@Before
+	public void initialiseAccount() {
+		accountMetier = new AccountMetier(statementMetier);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void initial_balance_should_not_be_null() {
+
 		new Account(null);
-	}
-
-	@Test
-	public void statement_should_not_be_null() throws BankAccountException {
-
-		/**
-		 * Given
-		 */
-		Account account = new Account(new Amount(100f));
-
-		/**
-		 * Then
-		 */
-		assertThat(account.statement()).isNotNull();
 
 	}
 
-	@Test(expected = BankAccountException.class)
-	public void deposited_amount_should_not_be_null() throws BankAccountException {
+	@Test(expected = NullPointerException.class)
+	public void statement_should_not_be_null() {
 
-		/**
-		 * Given
-		 */
-		Account account = new Account(new Amount(100f));
+		new AccountMetier(null);
 
-		/**
-		 * When
-		 */
-		account.deposit(null);
 	}
 
-	@Test(expected = BankAccountException.class)
-	public void withdrawal_amount_should_not_be_null() throws BankAccountException {
+	@Test(expected = NullPointerException.class)
+	public void deposited_amount_should_not_be_null() {
 
-		/**
-		 * Given
-		 */
-		Account account = new Account(new Amount(100f));
-
-		/**
-		 * When
-		 */
-		account.withdrawal(null);
+		accountMetier.deposit(null);
 	}
 
 	@Test()
-	public void deposited_amount_should_be_added_to_the_current_balance() throws BankAccountException {
+	public void deposited_amount_should_be_added_to_the_current_balance() {
 
 		/**
 		 * Given
 		 */
-		Account account = new Account(new Amount(100f));
-		Amount amount = new Amount(100f);
+		Amount balance = new Amount(100F);
+		Amount depositAmount = new Amount(100F);
 
 		/**
 		 * When
 		 */
-		account.deposit(amount);
+		accountMetier.deposit(depositAmount);
 
 		/**
 		 * Then
 		 */
-		assertThat(account.totalBalance()).isEqualTo(new Amount(200f));
+		verify(statementMetier).saveTransaction(any(TransactionPersistance.class), eq(balance));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void withdrawal_amount_should_not_be_null() {
+
+		accountMetier.withdrawal(null);
 
 	}
 
 	@Test()
-	public void withdrawal_amount_should_be_subtracted_from_the_current_amount() throws BankAccountException {
+	public void withdrawal_amount_should_be_subtracted_from_the_current_amount() {
 
 		/**
 		 * Given
 		 */
-		Account account = new Account(new Amount(300f));
-		Amount amount = new Amount(100f);
+		Amount depositAmount = new Amount(1000F);
+		Amount withdrawAmount = new Amount(400F);
+		Amount balance = new Amount(600F);
 
 		/**
 		 * When
 		 */
-		account.withdrawal(amount);
+		accountMetier.deposit(depositAmount);
+		accountMetier.withdrawal(withdrawAmount);
 
 		/**
 		 * Then
 		 */
-		assertThat(account.totalBalance()).isEqualTo(new Amount(200f));
+		verify(statementMetier).saveTransaction(any(TransactionPersistance.class), eq(balance));
+
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void print_should_not_be_null() {
+
+		accountMetier.print(null);
+
+	}
+
+	public void should_print() {
+
+		/**
+		 * When
+		 */
+		accountMetier.print(print);
+
+		/**
+		 * Then
+		 */
+		verify(statementMetier).print(print);
 
 	}
 
